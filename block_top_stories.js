a0284b2c23fbd2b5b427796b2175461a1d36b2e6
David A. Black <dblack@rubypal.com>
2016-04-07 06:37:23 -0400

    Convert tabs to 2 spaces

--- block_top_stories.js
+++ block_top_stories.js
@@ -1,11 +1,11 @@
 function checkUrl(url) {
-	return url.match(/www.facebook.com\/?(\?sk=h_nor)?$/) &&! url.match(/sk=h_chr/);
+  return url.match(/www.facebook.com\/?(\?sk=h_nor)?$/) &&! url.match(/sk=h_chr/);
 }
 
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
-	if (changeInfo.status == 'complete') {
-		if (checkUrl(tab.url)) {
-			chrome.tabs.update(tab.id, { url: 'https://www.facebook.com/?sk=h_chr' });
-		}
-	}
+  if (changeInfo.status == 'complete') {
+    if (checkUrl(tab.url)) {
+      chrome.tabs.update(tab.id, { url: 'https://www.facebook.com/?sk=h_chr' });
+    }
+  }
 });
