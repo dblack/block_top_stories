82d1683bc3926ad0f4946530590bb723ab086b9b
David A. Black <david@davidablack.net>
2022-11-09 13:43:17 -0500

    Update for manifest v3

--- block_top_stories.js
+++ block_top_stories.js
@@ -1,11 +1,17 @@
 function checkUrl(url) {
-  return url.match(/www.facebook.com\/?(\?sk=h_nor)?$/) &&! url.match(/sk=h_chr/);
+  return url.match(/(www\.)?facebook.com\/?(\?sk=h_nor)?$/) && !url.match(/sk=h_chr/);
 }
 
-chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
-  if (changeInfo.status == 'complete') {
-    if (checkUrl(tab.url)) {
-      chrome.tabs.update(tab.id, { url: 'https://www.facebook.com/?sk=h_chr' });
-    }
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.tabs.onUpdated.addListener(async () => {
+  var tab = await getCurrentTab();
+  var url = tab.url;
+  if (checkUrl(url)) {
+    await chrome.tabs.update(tab.id, { url: 'https://www.facebook.com/?sk=h_chr' });
   }
-});
+})
